0dcad1122fcdc6a4783edf3ef9947cf3710d9cc4
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-03 11:25:22 -0300

    Creating nubank header

--- Nubank/src/components/Header/index.js
+++ Nubank/src/components/Header/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-import {Container} from './styles';
+import {Container, Top, Logo, Title} from './styles';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import logo from '~/assets/Nubank_Logo.png';
 
 export default function header() {
-  return(
-    <Container />
+  return (
+    <Container>
+      <Top>
+        <Logo source={logo} />
+        <Title>Thadeu</Title>
+      </Top>
+      <Icon name='keyboard-arrow-down' type='MaterialIcons' size={20} color="#fff" />
+    </Container>
   );
 }
