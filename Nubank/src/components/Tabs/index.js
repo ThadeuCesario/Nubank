84c89e9b1584ca0520233cf3a59c4537a9a84874
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-03 12:39:45 -0300

    Creating our first tab

--- Nubank/src/components/Tabs/index.js
+++ Nubank/src/components/Tabs/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
-import {Container} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, TabsContainer, TabItem, TabText} from './styles';
 
 export default function header() {
-  return(
-    <Container />
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#fff" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
   );
 }
